7869c7334d8d112b701124de526d1ce64ea637f1
unknown <Vishal.srivastava@NDI-LAP-309.3pillar.corp>
2015-07-08 13:31:34 -0400

    new project

--- JenkinsTest/JenkinsTest/SekeniumTest.cs
+++ JenkinsTest/JenkinsTest/SekeniumTest.cs
@@ -2,10 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using NUnit;
+using System.Configuration;
+using System.Collections;
+using System.IO;
+using System.Data;
+
 
 namespace JenkinsTest
 {
     public class SekeniumTest
     {
+        IWebDriver driver;
+        private string Chrome_Driver = @Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + "\\chromedriver";
+        public static string browser = System.Environment.GetEnvironmentVariable("Browser");
+        public static string pageUrl = System.Environment.GetEnvironmentVariable("Page");
+        public IWebDriver getDriver(string browserType)
+        {
+            switch (browserType.ToLower())
+            {
+                   
+                case "chrome":
+                     var options = new ChromeOptions();
+                            driver = new ChromeDriver(Chrome_Driver);
+                    break;
+                case "firefox" :
+                    driver = new FirefoxDriver();
+                    break;
+
+            }
+            return driver;
+        }
+
+        [Test]
+        public void OpenGooglePage()
+        {
+            driver = getDriver(browser);
+            driver.Navigate().GoToUrl(pageUrl);
+        }
+        [Test]
+        public void OpenYahooPage()
+        {
+            driver = getDriver(browser);
+            driver.Navigate().GoToUrl("www.yahoo.com");
+        }
+ 
     }
 }
