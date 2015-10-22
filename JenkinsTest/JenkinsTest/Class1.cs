f093cf40692e41cf80197af82824da723bfa3210
“vishal <Vishal.srivastava@NDI-LAP-309.3pillar.corp>
2015-10-22 15:43:11 -0400

    testing Signed-off-by: “vishal <Vishal.srivastava@NDI-LAP-309.3pillar.corp>

---
+++ JenkinsTest/JenkinsTest/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JenkinsTest
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            SekeniumTest test = new SekeniumTest();
+            test.OpenGooglePage();
+        }
+    }
+}
